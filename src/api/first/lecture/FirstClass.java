e70b89b520b19e067ce6b59387b9b92c6c038d2c
RezaiH <117863882+RezaiH@users.noreply.github.com>
2024-03-11 00:17:31 -0400

    Update FirstClass.java

--- src/api/first/lecture/FirstClass.java
+++ src/api/first/lecture/FirstClass.java
@@ -5,4 +5,8 @@ public class FirstClass {
 	public static void main(String args[]) {
 		System.out.println("First change in the working directory.");
 	}
+
+		public static void showMethod() {
+		System.out.println("some changes made in the GitHub remote repository.");
+	}
 }
