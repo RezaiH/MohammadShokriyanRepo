628e97e978ecdd9de4499c3bc0ecbaaa673d3610
Hussain Rezai <rezaih637@gmail.com>
2024-03-11 00:20:56 -0400

    second class is added

---
+++ src/api/first/lecture/SecondClass.java
@@ -0,0 +1,11 @@
+package api.first.lecture;
+
+public class SecondClass {
+
+	public String showInfo() {
+		String name = "Hussain";
+		String lName = "Rezai";
+		int age = 28;
+		return name+" "+lName+" is "+age+" years old.";
+	}
+}
